0363425d7598c46d6b29c350752a5125c226ebf8
Hari Narayan <harinarayanmr@aol.com>
2024-12-17 15:36:44 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,94 @@
+<main>
+    <h1>National Higher Secondary School</h1>
+    <nav>
+        <li>
+            <a href="#">Home</a>
+            <a href="#">Gallery</a>
+            <a href="#">Info</a>
+        </li>
+    </nav>
+    <br>
+    <img
+        src="http://www.nationalhss.in/img/backgrounds/bg-14.jpg"
+        alt="header image"
+    />
+
+    <h1>About</h1>
+    <p>
+        At NHSS, we seek to blend and balance the best of traditional
+        educational and cultural values with established modern teaching
+        methodology, supported by the latest classroom technology. We identify
+        and celebrate the singularity of every child and strive to hone, develop
+        and perfect the skill sets required to reach pinnacles of success; be it
+        in academics, sports or fine arts. No wonder, the school has a proven
+        track record of producing outstanding leaders in all walks of life. We
+        always try to impart holistic education based on inclusiveness, empathy
+        and patriotism .
+    </p>
+    <h1>History</h1>
+    <p>
+        National Higher Secondary School, situated in the heart of Irinjalakuda,
+        a city famous as the birth place of World Famous
+        Mathematician-Astronomer Sangamagrama Madhavan (who founded the Kerala
+        school of astronomy and mathematics), the famous carnatic singer and
+        actress Nagapattinam Chandrashekhara Vasanthakokilam), the famous author
+        of Nalacharitham aattakatha Unnayi Warrier, who is known to have made
+        significant contributions to the art of Kathakali, the classical
+        dance-drama of Kerala, Koodiyattom Mastero Ammannur Madhava Chakyar
+        etc...<br /><br />
+
+        The school was founded in the year 1935 and is now completing 87 years
+        of dedicated service. The school celebrated its Platinum Jubilee in
+        2011. In 1942, the first batch of students appeared for the 7 th
+        standard public examination. From 1944 to 1991, Sri V K Menon was the
+        respected Manager of this school. With his sincere and relentless
+        efforts, the school achieved pre eminence in all fields. In 1991-1992,
+        Sri V K Menon handed over Management of the school to his daughter
+        Smt.Rukmani Ramachandran due to his failing health. He passed away in
+        2007.<br /><br />
+
+        Since taking over the management of school, Smt.Rukmani Ramachandran and
+        her husband and Management Representative Sri.V P R Menon are constant
+        sources of inspiration, guidance and leadership. Now the school is
+        steadily progressing under the strong leadership of Smt. Rukmani
+        Ramachandran and Sri. V P R Menon.<br /><br />
+
+        The Higher Secondary Section of th is school started in 1998. From 2005
+        to 2019, Smt Mini Mohandas was the Principal of this school. There are
+        LP, UP, High School and Higher Secondary Sections in this school. In
+        addition to academic activities, the school is one of the bestschools
+        for extra curricular activities. In the High school section, there are
+        NCC,SPC and JRC wings and in the higher secondary section, a strong NSS
+        team, Souhrida Club, Career Guidance and Seed Wing are all very much
+        active. At present, Smt Lisha V V is the principal and Smt. Sudha K K is
+        the Headmistress and both are providing able leadership for maintaining
+        the high standards the school has set for itself.
+    </p>
+    <h1>Vision</h1>
+    <p>
+        It is our vision to create a generation of Good people with cultural
+        values, indomitable intelligence and ultimately a good human being.
+    </p>
+    <h1>Mission</h1>
+    <p>
+        Our mission is to prepare young ones to pursue and achieve their
+        aspirations and contribute to the betterment of society.
+    </p>
+</main>
+
+<style>
+    main {
+        margin: 2rem auto;
+        width: max(60%, 350px);
+    }
+    li {
+        list-style: none;
+    }
+
+    a {
+        color: inherit;
+    }
+    img {
+        width: 100%;
+    }
+</style>
---
+++ src/readme.md
@@ -0,0 +1,6 @@
+# todo
+- mimic the same page (better UI)
+- implement real-time editing
+- advertise
+- attendance setting
+- teacher's old plan
